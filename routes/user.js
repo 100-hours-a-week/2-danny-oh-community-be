4cb9155f33cbb0283d6539b311b43029a24f5743
YEACHAN OH <dus001228@naver.com>
2024-11-10 21:17:44 +0900

    feat: 로그인 확인 미들웨어 구현

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
 
-
-router.get('/', userController.getUsers);
-router.patch('/', userController.updateUserProfile);
-router.patch('/password', userController.updateUserPass);
-router.delete('/', userController.deleteUser);
+router.get('/',authMiddleware, userController.getUser);
+router.patch('/',authMiddleware, userController.updateUserProfile);
+router.patch('/password',authMiddleware, userController.updateUserPass);
+router.delete('/',authMiddleware, userController.deleteUser);
 
 
 module.exports = router;
