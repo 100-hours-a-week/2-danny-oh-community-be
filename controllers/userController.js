61f48b6f72fad2e9ea96a580c970b4df789abb8f
YEACHAN OH <dus001228@naver.com>
2024-11-10 22:33:49 +0900

    feat: 프로필사진 닉네임 수정, 비밀번호수정, 사용자 삭제 기능 추가

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const userModel = require('../models/userModel');
+const path = require('path');
+const fs = require('fs');
 
 const getUser = (req, res) => {
     try {
@@ -19,6 +21,65 @@ const logout = (req, res) => {
         res.status(500).json({ message: '서버 오류가 발생했습니다.' });
     }
 }
+
+const updateUserProfile = async (req, res) => {
+    try {
+        const { email } = req.session.user;  // user 객체에서 email 추출
+        const { nickname } = req.body;
+        // 이미지 경로 처리
+        const profileImage = req.file ? req.file.path.replace(/\\/g, '/') : null;
+
+        // 사용자 정보를 업데이트하기 전에 기존 이미지 파일과 비교
+        if (profileImage && req.session.user.profileImage && profileImage !== req.session.user.profileImage) {
+            // 기존 이미지 경로가 존재하고, 새 이미지 경로가 다르면 기존 파일 삭제
+            const oldProfileImagePath = path.join(__dirname, '..', req.session.user.profileImage);
+            if (fs.existsSync(oldProfileImagePath)) {
+                fs.unlinkSync(oldProfileImagePath);  // 기존 파일 삭제
+            }
+        }
+
+        // 프로필 업데이트
+        userModel.updateProfile(email, nickname, profileImage);  
+
+        // 세션에 최신 정보 반영
+        req.session.user.nickname = nickname;
+        req.session.user.profileImage = profileImage;
+
+        res.status(200).json({ message: '프로필이 업데이트되었습니다.' });
+    } catch (error) {
+        console.error('프로필 업데이트 오류:', error);
+        res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    }
+};
+
+
+
+// 사용자 비밀번호 업데이트
+const updateUserPass= (req, res) => {
+    try {
+        const { email } = req.session.user;
+        const { newPassword } = req.body;
+        userModel.updatePassword(email, newPassword);
+        res.status(200).json({ message: '비밀번호가 업데이트되었습니다.' });
+    } catch (error) {
+        console.error('비밀번호 업데이트 오류:', error);
+        res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    }
+};
+
+// 사용자 삭제
+const deleteUser= (req, res) => {
+    try {
+        const { email } = req.session.user;
+        userModel.deleteUser(email);
+        req.session.destroy(); // 세션 삭제
+        res.status(200).json({ message: '사용자 계정이 삭제되었습니다.' });
+    } catch (error) {
+        console.error('사용자 삭제 오류:', error);
+        res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+    }
+};
+
 
 
 module.exports = { getUser, updateUserProfile, updateUserPass, deleteUser, logout };
