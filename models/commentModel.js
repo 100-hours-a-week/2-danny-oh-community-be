5ca570438ca3faa41130449ed47ec9360f61cbbb
YEACHAN OH <dus001228@naver.com>
2024-11-13 11:48:46 +0900

    feat: 댓글 데이터 컨트롤 모델 구현

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,26 +1,37 @@
 const postsFilePath = path.join(__dirname, '../posts.json'); // 게시글 JSON 파일 경로
 
-// 게시글 데이터를 저장하는 함수
+// 댓글 데이터를 저장하는 함수
 function savePostsData(posts) {
     fs.writeFileSync(postsFilePath, JSON.stringify(posts, null, 2));
 }
 
-// 댓글 추가 함수
-function addComment(postId, comment) {
+function getCommentById(post_id, comment_id){
     const posts = loadPostsData();
-    const postIndex = posts.findIndex(post => post.post_id === postId);
+    const postIndex = posts.findIndex(post => post.post_id === post_id);
+    return posts[postIndex].comments.find(comment => comment.comment_id === comment_id);
+}
+// 특정 게시글 조회 함수
+function getPostById(postId) {
+    const posts = loadPostsData();
+    return posts.find(post => post.post_id === postId);
+}
+
+// 댓글 추가 함수
+function addComment(post_id, comment) {
+    const posts = loadPostsData();
+    const postIndex = posts.findIndex(post => post.post_id === post_id);
     if (postIndex !== -1) {
         posts[postIndex].comments.push(comment)
         savePostsData(posts);
-        return posts[postIndex].comments;
+        return posts[postIndex].comment;
     }
     return null;
 }
 
 // 댓글 수정 함수
-function editComment(postId, comment_id, comment){
+function editComment(post_id, comment_id, comment){
     const posts = loadPostsData();
-    const postIndex = posts.findIndex(post => post.post_id === postId);
+    const postIndex = posts.findIndex(post => post.post_id === post_id);
     if (postIndex !== -1) {
         const commentIndex = posts[postIndex].comments.findIndex(comment => comment.comment_id === comment_id);
         posts[postIndex].comments[commentIndex].content = comment
@@ -31,9 +42,9 @@ function editComment(postId, comment_id, comment){
 }
 
 // 댓글 삭제 함수
-function deleteComment(postId, comment_id){
+function deleteComment(post_id, comment_id){
     const posts = loadPostsData();
-    const postIndex = posts.findIndex(post => post.post_id === postId);
+    const postIndex = posts.findIndex(post => post.post_id === post_id);
     const initialLength = posts[postIndex].comments.length;
     posts[postIndex].comments = posts[postIndex].comments.filter(comment => comment.comment_id !== comment_id);
     if (posts[postIndex].comments.length < initialLength) {
@@ -42,3 +53,11 @@ function deleteComment(postId, comment_id){
     }
     return null;
 }
+
+
+module.exports = {
+    getCommentById,
+    addComment,
+    editComment,
+    deleteComment
+};
