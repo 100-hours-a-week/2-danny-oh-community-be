dd79eb565c8723d69ab38bb03d6c111b28bab114
YEACHAN OH <dus001228@naver.com>
2024-11-18 17:44:44 +0900

    feat: cors 정책 적용 및 포트 변경

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const fs = require('fs');
+const cors = require('cors');  // CommonJS 방식으로 cors를 불러옴
 
 // 업로드 디렉토리 생성
 const uploadDir1 = path.join(__dirname, 'uploads/profileImages');
@@ -23,7 +24,7 @@ const postRoutes = require('./routes/post');
 const commentRoutes = require('./routes/comment');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,8 +33,18 @@ app.use(session({
     secret: 'mySecretKey',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, maxAge: 10 * 60 * 1000 }
+    cookie: { 
+        secure: false, // 로컬 환경에서는 false로 설정
+        httpOnly: true, // 자바스크립트로 쿠키를 접근할 수 없도록 설정
+        maxAge: 60 * 60 * 1000 // 쿠키 만료 시간 설정
+    }
 }));
+// cors 설정
+app.use(cors({
+    origin: 'http://localhost:3000',  // 클라이언트 도메인
+    credentials: true  // 쿠키를 포함한 요청 허용
+}));
+
 // 업로드된 이미지 제공 API
 const uploadDir = path.join(__dirname, 'uploads');
 
