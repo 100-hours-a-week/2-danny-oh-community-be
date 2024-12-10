fa40b97568150d0364abe2988312b72eab097bcf
YEACHAN OH <dus001228@naver.com>
2024-12-10 10:35:55 +0900

    feat: cors 수정

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
-import fs from 'fs';
 import cors from 'cors';
 
 import authRoutes from './routes/auth.js';
@@ -13,22 +10,6 @@ import commentRoutes from './routes/comment.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-// __dirname 설정 (ES 모듈 환경에서 __dirname 사용)
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// 업로드 디렉토리 생성
-// const uploadDir1 = path.join(__dirname, 'uploads/profileImages');
-// const uploadDir2 = path.join(__dirname, 'uploads/postImages');
-
-// if (!fs.existsSync(uploadDir1)) {
-//     fs.mkdirSync(uploadDir1, { recursive: true });
-// }
-
-// if (!fs.existsSync(uploadDir2)) {
-//     fs.mkdirSync(uploadDir2, { recursive: true });
-// }
 
 const app = express();
 const PORT = 8000;
@@ -52,16 +33,11 @@ app.use(
 // cors 설정
 app.use(
     cors({
-        origin: `http://${process.env.ADDRESS}:3000`, // 클라이언트 도메인
+        origin: `http://${process.env.ADDRESS}`, // 클라이언트 도메인
         credentials: true, // 쿠키를 포함한 요청 허용
     })
 );
 
-// 업로드된 이미지 제공 API
-// const uploadDir = path.join(__dirname, 'uploads');
-
-// 라우트 설정
-// app.use('/uploads', express.static(uploadDir));
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 app.use('/posts', postRoutes);
